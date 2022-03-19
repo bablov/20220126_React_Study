5010d14fd3380c1b81748d5065b2b76319664563
Baebae(@SP6) <1157075903@qq.com>
2022-03-19 16:55:19 +0800

    配置代理的第二种方式

---
+++ 02_staging/04_src_配置代理/index.js
@@ -0,0 +1,9 @@
+//引入react核心库
+import React from "react";
+//引入ReactDOM
+import ReactDOM from "react-dom";
+//引入App
+import App from './App'
+
+//渲染App到当前页面上
+ReactDOM.render(<App/>,document.getElementById('root'))
